14ff585130a34bb20e2cb2c31f86238ff6d29f8f
ittrainbow <50226581+ittrainbow@users.noreply.github.com>
2022-09-06 02:30:48 +0300

    alpha4

--- src/Components/ForgotPassword/ForgotPassword.js
+++ src/Components/ForgotPassword/ForgotPassword.js
@@ -1,7 +1,7 @@
 import React from 'react'
+import firebase from 'firebase/compat/app'
 import { useNavigate } from 'react-router-dom'
 import { connect } from 'react-redux'
-import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
 
