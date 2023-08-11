5f6342249ee923e8f97f39877cf6ea018dba8d99
Andrew Gordienko <50226581+ittrainbow@users.noreply.github.com>
2023-08-11 16:56:06 +0300

    swipe to custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -5,62 +5,20 @@ import { useNavigate } from 'react-router-dom'
 import { isMobile } from 'react-device-detect'
 
 import { INIT_APP, USER_LOGIN } from './redux/storetypes'
-import { initialRedirects, swipeHelper } from './helpers'
-import { selectApp, selectUser } from './redux/selectors'
+import { initialRedirects } from './helpers'
+import { selectApp } from './redux/selectors'
 import { appActions, userActions } from './redux/slices'
-import { getMenu } from './helpers/links'
 import { Header } from './pages'
 import { auth } from './db'
+import { useSwipe } from './hooks/useSwipe'
 
 export const App = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
-  const { emailReg, tabActive, duration, editor } = useSelector(selectApp)
-  const { admin } = useSelector(selectUser)
+  const { emailReg } = useSelector(selectApp)
   const [user] = useAuthState(auth)
 
-  // swipe
-
-  useEffect(() => {
-    let startX: number
-    let startY: number
-
-    const listenerStart = (e: TouchEvent) => {
-      startX = Math.round(e.touches[0].clientX)
-      startY = Math.round(e.touches[0].clientY)
-    }
-
-    const listenerEnd = (e: TouchEvent) => {
-      const endX = Math.round(e.changedTouches[0].clientX)
-      const endY = Math.round(e.changedTouches[0].clientY)
-      const moveX = endX - startX
-      const isSwipe = Math.abs(moveX) > 90 && Math.abs(endY - startY) < 45
-
-      if (isSwipe) {
-        const limit = admin ? 6 : 4
-        const canSwipeLeft = tabActive > 0
-        const canSwipeRight = tabActive < limit
-        const newTabActive =
-          moveX < 0 ? (canSwipeRight ? tabActive + 1 : tabActive) : canSwipeLeft ? tabActive - 1 : tabActive
-
-        swipeHelper({ moveX, canSwipeLeft, canSwipeRight })
-
-        const menu = getMenu(admin)
-        newTabActive === 5 && !editor && dispatch(appActions.setEditor(true))
-        newTabActive === 4 && editor && dispatch(appActions.setEditor(false))
-        dispatch(appActions.setTabActive(newTabActive))
-        setTimeout(() => navigate(menu[newTabActive].path), duration)
-      }
-    }
-
-    document.addEventListener('touchstart', listenerStart)
-    document.addEventListener('touchend', listenerEnd)
-
-    return () => {
-      document.removeEventListener('touchstart', listenerStart)
-      document.removeEventListener('touchend', listenerEnd)
-    } // eslint-disable-next-line
-  }, [tabActive, admin])
+  useSwipe()
 
   useEffect(() => {
     const lastTab = Number(localStorage.getItem('packContestLastTab') || 1)
---
+++ src/hooks/useSwipe.ts
@@ -0,0 +1,57 @@
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+import { selectApp, selectUser } from '../redux/selectors'
+import { appActions } from '../redux/slices'
+import { swipeHelper, getMenu } from '../helpers'
+
+export const useSwipe = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const { tabActive, duration, editor } = useSelector(selectApp)
+  const { admin } = useSelector(selectUser)
+
+  useEffect(() => {
+    let startX: number
+    let startY: number
+
+    const listenerStart = (e: TouchEvent) => {
+      startX = Math.round(e.touches[0].clientX)
+      startY = Math.round(e.touches[0].clientY)
+    }
+
+    const listenerEnd = (e: TouchEvent) => {
+      const endX = Math.round(e.changedTouches[0].clientX)
+      const endY = Math.round(e.changedTouches[0].clientY)
+      const moveX = endX - startX
+      const isSwipe = Math.abs(moveX) > 90 && Math.abs(endY - startY) < 45
+
+      if (isSwipe) {
+        const limit = admin ? 6 : 4
+        const canSwipeLeft = tabActive > 0
+        const canSwipeRight = tabActive < limit
+        const newTabActive =
+          moveX < 0 ? (canSwipeRight ? tabActive + 1 : tabActive) : canSwipeLeft ? tabActive - 1 : tabActive
+
+        swipeHelper({ moveX, canSwipeLeft, canSwipeRight })
+
+        const menu = getMenu(admin)
+        newTabActive === 5 && !editor && dispatch(appActions.setEditor(true))
+        newTabActive === 4 && editor && dispatch(appActions.setEditor(false))
+        dispatch(appActions.setTabActive(newTabActive))
+        setTimeout(() => navigate(menu[newTabActive].path), duration)
+      }
+    }
+
+    document.addEventListener('touchstart', listenerStart)
+    document.addEventListener('touchend', listenerEnd)
+
+    return () => {
+      document.removeEventListener('touchstart', listenerStart)
+      document.removeEventListener('touchend', listenerEnd)
+    } // eslint-disable-next-line
+  }, [tabActive, admin])
+
+  return
+}
