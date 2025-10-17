b5aa4251a69d0a093022f825d3c96ed2ec861393
Andrey Gordienko <andreygordieko@b2c.ttk.ru>
2025-10-17 23:48:12 +0300

    fading-re-do

--- src/hooks/use-fade.ts
+++ src/hooks/use-fade.ts
@@ -8,13 +8,16 @@ export function useFade(ref: React.RefObject<HTMLDivElement>) {
   const triggerFade = () => {
     const list = ref.current?.classList
 
-    list?.remove('animate-fade-in-up')
     list?.add('animate-fade-out-down')
 
     setTimeout(() => {
       list?.remove('animate-fade-out-down')
       list?.add('animate-fade-in-up')
     }, duration)
+
+    setTimeout(() => {
+      list?.remove('animate-fade-in-up')
+    }, duration * 2)
   }
 
   return triggerFade
